5f0e389f8f072ba9c513639eb05472b045978934
JENNIFER DE JESUS BRITO ROMERO <jenniferj.brirom@educa.jcyl.es>
2026-03-03 20:35:22 +0100

    feat: Creada la clase EmailService e implementada.

---
+++ P4.1-notifications-base/EmailService.java
@@ -0,0 +1,44 @@
+public class EmailService implements NotificationService{
+    //ATRIBUTOS:
+    private String type;
+    private String message;
+    private String recipient;
+    
+
+    //CONSTRUCTOR
+    public EmailService(String type, String message, String recipient) {
+        this.type = type;
+        this.message = message;
+        this.recipient = recipient;
+    }
+
+    //GETTERS AND SETTERS
+    public String getType() {   return type;    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {    return message; }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getRecipient() {  return recipient;   }
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    
+
+    //MÉTODOS DE LA CLASE
+    //TODO: CÓDIGO PARA ENVIAR EMAIL
+    
+    @Override
+    public void sendMultiple(String message, String... recipient) {
+        System.out.println("Enviando mensaje a " + recipient.toString());
+        System.out.println("Mensaje: " + message);
+    }
+
+ 
+
+}
